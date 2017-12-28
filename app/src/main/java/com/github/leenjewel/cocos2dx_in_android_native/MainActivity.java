241a1e37d9e578ba91c4092bd7fe1177be98de78
liyin <liy@yomob.com>
2017-12-28 15:52:11 +0800

    将 cocos2d-x 引擎作为控件引入 native 工程主 Activity 中

--- app/src/main/java/com/github/leenjewel/cocos2dx_in_android_native/MainActivity.java
+++ app/src/main/java/com/github/leenjewel/cocos2dx_in_android_native/MainActivity.java
@@ -1,9 +1,19 @@
 package com.github.leenjewel.cocos2dx_in_android_native;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
+import org.cocos2dx.lib.Cocos2dxHelper;
+import org.cocos2dx.lib.Cocos2dxRenderer;
+
+public class MainActivity extends AppCompatActivity implements Cocos2dxHelper.Cocos2dxHelperListener {
+
+    private Cocos2dxGLSurfaceView mGLSurfaceView = null;
+    private int[] mGLContextAttrs = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +25,43 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        Cocos2dxActivity.setContext(this);
+
+        Cocos2dxHelper.init(this);
+
+        this.mGLContextAttrs = Cocos2dxActivity.getGLContextAttrs();
+
+        this.mGLSurfaceView = (Cocos2dxGLSurfaceView) this.findViewById(
+                R.id.cocos2d_gl_surface_view);
+
+        if (this.mGLContextAttrs[3] > 0) {
+            mGLSurfaceView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        }
+
+        Cocos2dxActivity.Cocos2dxEGLConfigChooser chooser = new Cocos2dxActivity.Cocos2dxEGLConfigChooser(
+                this.mGLContextAttrs);
+        this.mGLSurfaceView.setEGLConfigChooser(chooser);
+        this.mGLSurfaceView.setCocos2dxRenderer(new Cocos2dxRenderer());
+        this.mGLSurfaceView.setZOrderOnTop(true);
+    }
+
+    @Override
+    public void showDialog(final String pTitle, final String pMessage) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                (new AlertDialog.Builder(MainActivity.this))
+                        .setMessage(pMessage)
+                        .setTitle(pTitle)
+                        .setPositiveButton("OK", null)
+                        .show();
+            }
+        });
+    }
+
+    @Override
+    public void runOnGLThread(Runnable pRunnable) {
+        this.mGLSurfaceView.queueEvent(pRunnable);
     }
 }
